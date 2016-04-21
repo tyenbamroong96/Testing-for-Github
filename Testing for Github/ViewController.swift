6945d65274ca55eb3275a20de6340a1c1dd234b2
Thomas Yenbamroong <tyenbamroong@Thomass-MacBook-Pro-2.local>
2016-04-21 18:03:55 +0100

    Created a new foleder called Testing for Github

---
+++ Testing for Github/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testing for Github
+//
+//  Created by Thomas Yenbamroong on 21/04/2016.
+//  Copyright © 2016 UCL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
